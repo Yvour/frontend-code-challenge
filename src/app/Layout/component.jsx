a0e6341ba8144d5800f9b47ef359f470dac2f0d1
Yvour <ivour@rambler.ru>
2018-01-05 10:12:48 +0300

    lines

--- src/app/Layout/component.jsx
+++ src/app/Layout/component.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import styles from "./style.css";
 import PropTypes from 'prop-types';
-
-
 export default class Layout extends
 React.Component {
-	render() {
-		return ( 
-		<div className="layout_container">
-		
-		<div className="purpose">{(this.props.isSell?"Kaufen":"Mieten")}</div>
+  render() {
+    return(
+      <div className="layout_container">		
+         <div className="purpose">{(this.props.isSell?"Kaufen":"Mieten")}</div>
 		<img className="photo" src={this.props.imgSrc}/>
-		
-
 		<div className="title">
 		<div className="title-text">
 		{this.props.title}<br/><br/>
@@ -22,23 +17,23 @@ React.Component {
 		{this.props.address}
 		</div>
 		<div className="bottom-info">
-	     	 <div className="price"><b>{`${this.props.price} €`}</b></div>
+	     	     <div className="price"><b>{`${this.props.price} €`}</b></div>
 		     <div className="numberOfRooms">{this.props.numberOfRooms} Zimmer</div>
 		     <div className="total-area">ab {this.props.totalArea}m<sup>2</sup></div>
 		</div>
 
 
 
-	</div>)
-	}
+	</div>
+    )
+  }
 }
-
 Layout.propTypes = {
-	title : PropTypes.string,
-	address : PropTypes.string,
-	price : PropTypes.number,
-	numberOfRooms : PropTypes.number,
-	totalArea : PropTypes.number,
-	imgSrc : PropTypes.string,
-	isSell: PropTypes.bool
+  title: PropTypes.string,
+  address: PropTypes.string,
+  price: PropTypes.number,
+  numberOfRooms: PropTypes.number,
+  totalArea: PropTypes.number,
+  imgSrc: PropTypes.string,
+  isSell: PropTypes.bool
 }
